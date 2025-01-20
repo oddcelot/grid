0f6788cbdb1312333e1045236cf90aad5d898578
Stefan Kopco <hey@oddcelot.dev>
2025-01-20 23:32:32 +0100

    use env path

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,10 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import solid from "vite-plugin-solid";
 
-export default defineConfig({
-  base: "/grid/",
-  plugins: [solid()],
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "");
+  return {
+    plugins: [solid()],
+    base: env.BASE_PATH,
+  };
 });
